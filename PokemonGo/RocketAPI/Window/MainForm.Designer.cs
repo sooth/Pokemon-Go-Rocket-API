e2123844ed6af83a2f4587706de5edabf0289c49
david.malson <david.malson@sigmanest.com>
2016-08-01 08:12:37 -0400

    Seth's Balls

--- PokemonGo/RocketAPI/Window/MainForm.Designer.cs
+++ PokemonGo/RocketAPI/Window/MainForm.Designer.cs
@@ -47,7 +47,7 @@
             // 
             // logTextBox
             // 
-            this.logTextBox.BackColor = System.Drawing.Color.Black;
+            this.logTextBox.BackColor = System.Drawing.Color.White;
             this.logTextBox.Dock = System.Windows.Forms.DockStyle.Fill;
             this.logTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.logTextBox.ForeColor = System.Drawing.Color.White;
@@ -91,7 +91,7 @@
             this.menuStrip1.TabIndex = 2;
             this.menuStrip1.Text = "menuStrip1";
             // 
-            // startBotToolStripMenuItem
+            // startStopBotToolStripMenuItem
             // 
             this.startStopBotToolStripMenuItem.Name = "startStopBotToolStripMenuItem";
             this.startStopBotToolStripMenuItem.Size = new System.Drawing.Size(64, 20);
